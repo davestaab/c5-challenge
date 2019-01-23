94b18ed0e890870e103de75eac91d46e485cb52c
Dave Staab <davestaab@gmail.com>
2019-01-23 16:37:23 -0500

    changed fastest overall to 2 stars

--- src/store/submissionStars.js
+++ src/store/submissionStars.js
@@ -9,7 +9,7 @@ const factory = (id, title, stars) => {
 export const ONE_SUCCESSFUL = factory(1, 'Successful Submission!', 1);
 export const TWO_SUCCESSFUL = factory(2, 'Two Submissions!', 3);
 export const THREE_SUCCESSFUL = factory(3, 'Three Submissions!', 5);
-export const SMALLEST_CAT = factory(4, 'Smallest in Category', 1);
+export const FASTEST_ALL = factory(7, 'Fastest Overall', 2);
 export const FASTEST_CAT = factory(5, 'Fastest in Category', 1);
 export const SMALLEST_ALL = factory(6, 'Smallest Overall', 1);
-export const FASTEST_ALL = factory(7, 'Fastest Overall', 1);
+export const SMALLEST_CAT = factory(4, 'Smallest in Category', 1);
