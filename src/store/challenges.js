a116ba226fd44b2aec8424e8e0caa6816beacfc9
Dave Staab <davestaab@gmail.com>
2019-01-24 15:19:47 -0500

    adding announcementDate and teamSubmissionDate

--- src/store/challenges.js
+++ src/store/challenges.js
@@ -3,10 +3,12 @@ export default [
     1,
     'Egg Drop',
     `given 2 eggs, determine what floor of a building the eggs can be dropped from without breaking`,
-    '2019-03-15'
+    '2019-03-15',
+    '2019-01-31',
+    '2019-01-31'
   )
 ];
 
-function factory(id, title, description, eventDate, submissions) {
-  return { id, title, description, eventDate, submissions };
+function factory(id, title, description, eventDate, announcementDate, teamSubmissionDate) {
+  return { id, title, description, eventDate, announcementDate, teamSubmissionDate };
 }
