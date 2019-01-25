f87f05ad7b86b7c6ae562bccbf57f1b0215e0d84
Dave Staab <davestaab@gmail.com>
2019-01-25 08:12:17 -0500

    refactored store and added state store

---
+++ src/store/state/index.js
@@ -0,0 +1,15 @@
+import challenges from './challenges';
+import people from './people';
+import submissions from './submissions';
+import submissionStars from './submissionStars';
+import teamChallenges from './teamChallenges';
+import teams from './teams';
+
+export default {
+  challenges,
+  people,
+  submissions,
+  submissionStars,
+  teamChallenges,
+  teams
+};
