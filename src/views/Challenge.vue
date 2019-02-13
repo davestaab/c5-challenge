49a6adff9e39bf97e3077fe022f730898e95785a
Dave Staab <davestaab@gmail.com>
2019-02-12 22:23:36 -0500

    styled router links in challenge page

--- src/views/Challenge.vue
+++ src/views/Challenge.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <router-link to="description">Description</router-link>&nbsp;
-    <router-link to="results">Results</router-link>
+    <ul class="list-reset flex">
+      <router-link to="description" tag="li" class="mr-6 p-2 border rounded border-grey-darker">
+        <a class="text-black font-bold hover:text-red">Description</a>
+      </router-link>
+      <router-link tag="li" to="results" class="mr-6 p-2 border rounded border-grey-darker">
+        <a class="text-black hover:text-red font-bold">Results</a>
+      </router-link>
+    </ul>
     <router-view></router-view>
   </div>
 </template>
@@ -11,3 +17,11 @@ export default {
   name: 'Challenge'
 };
 </script>
+<style scoped>
+.router-link-active {
+  border-color: red;
+}
+.router-link-active a {
+  /*color: red;*/
+}
+</style>
