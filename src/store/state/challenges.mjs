240661c1fbe6934a125c22cc0ee3397b6ce2929a
Dave Staab <davestaab@gmail.com>
2019-04-08 13:13:59 -0400

    adding challenge 2 description

--- src/store/state/challenges.mjs
+++ src/store/state/challenges.mjs
@@ -1,11 +1,6 @@
 export default [
-  factory(
-    1,
-    'The Two Egg Problem',
-    '2019-02-14',
-    '2019-01-31',
-    '2019-01-31'
-  )
+  factory(1, 'The Two Egg Problem', '2019-02-14', '2019-01-31', '2019-01-31'),
+  factory(2, 'Easter Bunny', '2019-04-22 12:00:00pm', '2019-04-08', '2019-04-12 11:59:59pm')
 ];
 
 function factory(id, title, eventDate, announcementDate, teamSubmissionDate) {
